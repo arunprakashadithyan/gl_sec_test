0e4feca55e0d502e4b6f8df7041d9ce41f38ceea
Arun Prakash Subramanian <arunprakash.s@Aruns-MacBook-Pro.local>
2023-11-03 12:28:07 +0530

    this code has secrets

---
+++ index.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const { response } = require('express');
+const dotenv = require('dotenv');
+
+
+(async () => {
+    const response = await axios.get('https://gitlab.com/api/v4/projects/46880399/merge_requests/3', {
+        headers: {
+            'PRIVATE-TOKEN': "glpat-SYnr3CfgFaWnnzDwfgVE",
+        }
+    })
+    console.log(response);
+})()
